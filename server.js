8b0a931d649e242a031cb77e6aeefc58feef5a70
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2014-04-27 21:42:17 +0100

    NoTask - refactored server.js, removed routes.js in favour of controllers, updated index view and bower packages

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
 'use strict';
 
+// add newrelic key
+// require('newrelic');
+
+// env configuration
+var env  = process.env.NODE_ENV || 'development';
+var port = process.env.PORT || 3000;
+
 // modules
 var express   = require('express'),
     exphbs    = require('express3-handlebars'),
     http      = require('http'),
     path      = require('path'),
-    winston   = require('winston');
+    logger    = require('./logger'),
+    app       = express();
 
-var app = express();
-
-// routes
-require('./routes')(app);
-
-// port handlebars engine
-app.set('port', process.env.PORT || 3000);
+// port  / handlebars engine
+app.set('port', port);
 app.engine('hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', 'hbs');
 
 // middleware
-app.use(express.logger({ immediate: true, format: 'dev' }));
 app.use(express.compress());
-app.use(express.bodyParser());        // pull information from html in POST
-app.use(express.methodOverride());    // simulate DELETE and PUT
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(require('winston-request-logger').create(logger));
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+// make her listen
+http.createServer(app).listen(app.get('port'));
 
-http.createServer(app).listen(app.get('port'), function(){
-  winston.info('Express server listening on port ' + app.get('port'));
-});
+module.exports.app = app;
+require('./routes');
